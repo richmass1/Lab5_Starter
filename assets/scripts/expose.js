dbe77df0185151d9e0600d9e2ce62525d4851170
richmass1 <103288353+richmass1@users.noreply.github.com>
2024-05-06 00:17:44 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,15 +4,22 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const dropdown = document.getElementById("horn-select");
-  const hornImage = document.querySelector("section > img");
-  const volumeIcon = document.querySelector("div > img");
-  const playButton = document.querySelector("section > button");
+  const hornImage = document.querySelector("section#expose > img");
+  const volumeIcon = document.querySelector("div#volume-controls > img");
+  const playButton = document.querySelector("section#expose > button");
+  const audioPlayer = document.querySelector("section#expose > audio");
+  const volumeSlider = document.querySelector("input[type='range']")
 
   dropdown.addEventListener("change", (event) => {
-    hornImage.src = "/assets/" + event.target.value + ".svg"
+    hornImage.src = "/assets/images/" + event.target.value + ".svg"
+    audioPlayer.src = "/assets/audio/" + event.target.value + ".mp3"
   });
 
   playButton.addEventListener("click", (event) => {
+    audioPlayer.play();
+  });
+
+  volumeSlider.addEventListener("change", (event) => {
     // TODO
   });
 }

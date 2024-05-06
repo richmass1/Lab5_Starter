d692dbadc43e6c348ebbd668a4dd45f39329ace8
richmass1 <103288353+richmass1@users.noreply.github.com>
2024-05-05 23:44:44 -0700

    begin lab 5

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,16 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const dropdown = document.getElementById("horn-select");
+  const hornImage = document.querySelector("section > img");
+  const volumeIcon = document.querySelector("div > img");
+  const playButton = document.querySelector("section > button");
+
+  dropdown.addEventListener("change", (event) => {
+    hornImage.src = "/assets/" + event.target.value + ".svg"
+  });
+
+  playButton.addEventListener("click", (event) => {
+    // TODO
+  });
 }

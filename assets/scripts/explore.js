e619619ef18f37b0fd73db0039bd6ec419039702
richmass1 <103288353+richmass1@users.noreply.github.com>
2024-05-06 21:08:04 -0700

    Explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const faceIMG = document.querySelector("section#explore > img");
+  const txtbox = document.getElementById("text-to-speak");
+  const dropdown = document.getElementById("voice-select");
+  const playButton = document.querySelector("section#explore > button");
+
+  // populate dropdown with voice options
+  let voicelist = [];
+  speechSynthesis.addEventListener("voiceschanged", () => {
+    voicelist = speechSynthesis.getVoices();
+    for (let i = 0; i < voicelist.length; i++) {
+    const voiceOption = new Option(voicelist[i].name, i);
+    dropdown.add(voiceOption);
+  }
+  });
+
+  playButton.addEventListener("click", (event) => {
+    faceIMG.src = "./assets/images/smiling-open.png";
+    let speech = new SpeechSynthesisUtterance(txtbox.value);
+    speech.voice = voicelist[dropdown.selectedOptions[0].value];
+    speechSynthesis.speak(speech);
+    speech.addEventListener("end", () => {
+      faceIMG.src = "./assets/images/smiling.png";
+    });
+  });
 }
